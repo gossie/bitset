64437eab94b7847b3da22691c2bf9e5d09df19ea
André Schreck <andre.schreck@gmail.com>
2021-01-16 13:33:00 +0100

    BitSet does not need to be a struct

--- bitset.go
+++ bitset.go
@@ -3,18 +3,16 @@ package bitset
 import "strings"
 
 // BitSet is a bitset.
-type BitSet struct {
-	data []byte
-}
+type BitSet []byte
 
 // From creates a new bitset from the given slice.
 func From(data []byte) BitSet {
-	return BitSet{data: data}
+	return data
 }
 
 func (b BitSet) String() string {
 	stringBuilder := strings.Builder{}
-	for _, bytes := range b.data {
+	for _, bytes := range b {
 		mask := byte(1)
 		for i := 0; i < 8; i++ {
 			if (bytes & (mask << i)) != 0 {
@@ -30,24 +28,24 @@ func (b BitSet) String() string {
 // IsSet returns true if the bit at the given index is set.
 func (b *BitSet) IsSet(index uint) bool {
 	dataIndex := index >> 3
-	if dataIndex >= uint(len(b.data)) {
+	if dataIndex >= uint(len(*b)) {
 		return false
 	}
 	value := 1 << (index & 7)
-	return (b.data[dataIndex] & byte(value)) > 0
+	return ((*b)[dataIndex] & byte(value)) > 0
 }
 
 // Set sets the bit at the given index.
 func (b *BitSet) Set(index uint) {
 	dataIndex := index >> 3
-	for i := uint(len(b.data)); i <= dataIndex; i++ {
-		b.data = append(b.data, 0)
+	for i := uint(len(*b)); i <= dataIndex; i++ {
+		*b = append(*b, 0)
 	}
 	value := 1 << (index & 7)
-	b.data[dataIndex] |= byte(value)
+	(*b)[dataIndex] |= byte(value)
 }
 
 // Bytes returns the a slice of byte.
 func (b *BitSet) Bytes() []byte {
-	return b.data
+	return *b
 }
